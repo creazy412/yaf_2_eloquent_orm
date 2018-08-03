325b45fdce9fa37a3191020bec469a09ee6f42a5
creazy412 <754634469@qq.com>
2018-08-03 11:11:10 +0800

    add comment
    add comment

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,11 +1,32 @@
 <?php
 
-use Yaf\Controller_Abstract;
-// use models\Yaf;
-// use library\MyPDO;
+/**
+ * 默认index
+ * 
+ * @category Xxx
+ * @package  Xxx
+ * @author   laok <laok518@126.com>
+ * @license  gun http://www.gun.org
+ * @link     Xxx
+ */
 
+use Yaf\Controller_Abstract;
+/**
+ * 默认的 Index 控制器
+ * 
+ * @category Xxx
+ * @package  Xxx
+ * @author   laok <laok518@126.com>
+ * @license  gun http://www.gun.org
+ * @link     Xxx
+ */
 class IndexController extends YAF_Controller_Abstract
 {
+    /**
+     * 默认Action操作
+     * 
+     * @return render
+     */
     public function indexAction()
     {
         // 默认Action
@@ -13,16 +34,26 @@ class IndexController extends YAF_Controller_Abstract
             ->assign('content', 'hello world-2');
     }
 
+    /**
+     * 使用 bootstrap 模版
+     * 
+     * @return render
+     */
     public function bootstrapAction()
     {
-		$result = UserModel::all();
-		// var_dump($result[0]['name']);
-		// die;
+        $result = UserModel::all();
+        // var_dump($result[0]['name']);
+        // die;
         // $db = MyPDO::getInstance('localhost', 'root', '123123', 'yaf', 'utf8');
         // $result = $db->query("select * from yaf");
         $this->getView()->assign("result", $result);
     }
 
+    /**
+     * 增加
+     * 
+     * @return render
+     */
     public function addAction()
     {
         if ($this->getRequest()->getMethod() == "POST") {
@@ -31,7 +62,7 @@ class IndexController extends YAF_Controller_Abstract
                 $insertResult = $db->insert("yaf", $_POST);
                 if ($insertResult) {
                     echo "成功";
-					$this->redirect("Index/bootstrap");
+                    $this->redirect("Index/bootstrap");
                 } else {
                     echo "失败";
                 }
@@ -42,6 +73,11 @@ class IndexController extends YAF_Controller_Abstract
         $this->getView();
     }
 
+    /**
+     * 编辑
+     * 
+     * @return render
+     */
     public function editAction()
     {
         $db = MyPDO::getInstance('localhost', 'root', '123123', 'yaf', 'utf8');
@@ -59,7 +95,7 @@ class IndexController extends YAF_Controller_Abstract
             } else {
                 header("Content-type:text/html;charset=utf-8");
                 echo('数据格式有误');
-				$this->redirect("Index/bootstrap");
+                $this->redirect("Index/bootstrap");
             }
         } else {
             $id = $_GET['id'];
@@ -71,13 +107,18 @@ class IndexController extends YAF_Controller_Abstract
         }
     }
 
+    /**
+     * 删除
+     * 
+     * @return render
+     */
     public function delAction()
     {
-		$db = MyPDO::getInstance('localhost', 'root', '123123', 'yaf', 'utf8');
-		if (!$_GET['id'] || !($db->query("select * from yaf where id = $_GET[id]"))) {
-		    die("用户不存在或数据有误");
-		}
-		$db->delete("yaf", "id = $_GET[id]");
+        $db = MyPDO::getInstance('localhost', 'root', '123123', 'yaf', 'utf8');
+        if (!$_GET['id'] || !($db->query("select * from yaf where id = $_GET[id]"))) {
+            die("用户不存在或数据有误");
+        }
+        $db->delete("yaf", "id = $_GET[id]");
         $this->redirect("Index/bootstrap");
     }
 }
